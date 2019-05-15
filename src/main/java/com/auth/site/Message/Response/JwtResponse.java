7ff80feda06669431c9840267e687abe1c3db503
Dennis Muturia <dmuturia2922@gmail.com>
2019-05-15 15:06:09 +0300

    Added an access token that will be returned by the application after a successful login.

--- src/main/java/com/auth/site/Message/Response/JwtResponse.java
+++ src/main/java/com/auth/site/Message/Response/JwtResponse.java
@@ -1,4 +1,26 @@
 package com.auth.site.Message.Response;
 
 public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+
+    //Constructor token
+    public JwtResponse(String accessToken){
+        this.token = accessToken;
+    }
+
+    public  String getAccessToken(){
+        return token;
+    }
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getTokenType() {
+        return type;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.type = tokenType;
+    }
 }
